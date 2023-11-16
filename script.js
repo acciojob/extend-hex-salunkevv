54cf8a818dcf9e4f09fe8590a9c41989ba945fc7
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-16 19:29:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let ans="#";
+
+	for(let i=0;i<4;i++)
+		{
+			if(shortHex.charAt(i)=="#")
+			{
+				continue;
+			}
+			else
+			{
+				ans= ans+shortHex.charAt(i)+shortHex.charAt(i);
+			}
+		}
+	return ans;
 };
 
 // Do not change the code below.
